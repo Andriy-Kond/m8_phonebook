f5d8974898f5847d005930daa87911088a1ab463
Andrii <andrii.kondratovich@gmail.com>
2024-10-10 13:25:05 +0300

    clear: some comments

--- src/common/pages/SharedLayout.jsx
+++ src/common/pages/SharedLayout.jsx
@@ -1,6 +1,6 @@
-import AppBar from "common/components/navigation/AppBar";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import AppBar from "common/components/navigation/AppBar";
 
 export default function SharedLayout() {
   return (
@@ -8,15 +8,6 @@ export default function SharedLayout() {
       <main>
         <header>
           <AppBar />
-          {/* <div style={{ display: "flex", gap: "10px" }}>
-            <Link to="/">Home</Link>
-            <Link to="/contacts">Contacts</Link>
-          </div> */}
-
-          {/* <div style={{ display: "flex", gap: "10px" }}>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Login</Link>
-          </div> */}
         </header>
 
         <Suspense fallback={<div>Loading...</div>}>
